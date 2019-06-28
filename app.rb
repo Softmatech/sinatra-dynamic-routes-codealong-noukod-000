612c00a5f87ae3453d1ea301995577d4565ebd1c
Andy Feidje Joseph <ingandy75@gmail.com>
2019-06-28 18:13:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -23,14 +23,10 @@ class App < Sinatra::Base
 
   get '/goodbye/:name' do
     @user_name = params[:name]
-    "Goodbye, #{@user_name}."
     erb :goodbye
   end
 
   get '/multiply/:num1/:num2' do
-  @num1 = params[:num1].to_i
-  @num2 = params[num2].to_i
-  "@result = (@num1 * @num2)"
   erb :multiply
 end
 end
